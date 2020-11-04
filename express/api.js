d9153ca1fd344343ac09fbfc31ff3604bb7d307b
Panadora <chenhuanhui123@gmail.com>
2020-11-04 16:07:11 +0700

    Update Click

--- express/api.js
+++ express/api.js
@@ -10,6 +10,10 @@ const router = express.Router();
 //database.connect();
 
 router.get('/', (req, res, next) => {
+  res.send('<h1><a href="/.netlify/functions/api/JSON">CLICK HERE - CONNECT JSON</a></h1>');
+});
+
+router.get('/JSON', (req, res) => {
   res.json(dbJson)
 });
 
